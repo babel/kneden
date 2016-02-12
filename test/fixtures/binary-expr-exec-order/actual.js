2d25fa64af66b9fe1a61ff03e4922ea8d734b2b8
Marten de Vries <m@rtendevri.es>
2016-02-12 20:18:38 +0100

    Coverage back to 100%

--- test/fixtures/binary-expr-exec-order/actual.js
+++ test/fixtures/binary-expr-exec-order/actual.js
@@ -1,3 +1,7 @@
+/* eslint no-empty: 0 */
+
 async function test() {
+  if(c() === await d()) {}
+
   return a() + await b();
 }
--- test/fixtures/binary-expr-exec-order/expected.js
+++ test/fixtures/binary-expr-exec-order/expected.js
@@ -1,10 +1,20 @@
+/* eslint no-empty: 0 */
+
 function test() {
-  var _temp;
+  var _temp, _temp2;
 
   return Promise.resolve().then(function () {
-    _temp = a();
+    return Promise.resolve().then(function () {
+      _temp = c();
+      return d();
+    }).then(function (_resp) {
+      return _temp === _resp;
+    });
+  }).then(function (_resp) {
+    if (_resp) {}
+    _temp2 = a();
     return b();
   }).then(function (_resp) {
-    return _temp + _resp;
+    return _temp2 + _resp;
   });
 }
